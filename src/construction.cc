99bc1c35518efaa21dd68f5c554741864dc5efe7
María <maria.rd02@gmail.com>
2024-10-26 20:23:52 +0200

    Added scintillation

--- src/construction.cc
+++ src/construction.cc
@@ -40,6 +40,29 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
 	CsIMaterial->AddElement(elI, .5);
 	CsIMaterial->AddElement(elCs,.5);
 
+	// Add the physical propierties of the scintillating material
+	std::vector<G4double> energy     = {2.034*eV, 3.*eV, 4.136*eV};
+	std::vector<G4double> rindex     = {1.58, 1.58, 1.58};
+	std::vector<G4double> absorption = {400.*cm, 400.*cm, 400.0*cm};
+	
+	G4MaterialPropertiesTable* MPT = new G4MaterialPropertiesTable();
+	
+	// property independent of energy
+	MPT->AddConstProperty("SCINTILLATIONYIELD", 9200./MeV);
+	
+	// properties that depend on energy
+	MPT->AddProperty("RINDEX", energy, rindex);
+	MPT->AddProperty("ABSLENGTH", energy, absorption);
+	
+	
+	const G4int NUMENTRIES = 13;
+	G4double Scnt_PP[NUMENTRIES] = { 2.39*eV, 2.43*eV, 2.48*eV, 2.53*eV, 2.58*eV, 2.64*eV, 2.70*eV, 2.76*eV, 2.82*eV, 2.89*eV, 2.95*eV, 3.03*eV, 3.10*eV};
+	G4double Scnt_FAST[NUMENTRIES] = {0.02, 0.05, 0.10, 0.20, 0.35, 0.50, 0.65, 0.85, 1.00, 0.85, 0.65, 0.35, 0.10};
+	MPT->AddProperty("SCINTILLATIONCOMPONENT1", Scnt_PP, Scnt_FAST, NUMENTRIES); // We only consider the fast component
+	MPT->AddConstProperty("RESOLUTIONSCALE", 2.0);
+	MPT->AddConstProperty("SCINTILLATIONTIMECONSTANT1", 3.3*ns); 
+	CsIMaterial->SetMaterialPropertiesTable(MPT);
+
 	// Define the solid and logical volume
 	G4Box *solidLog = new G4Box("solidLog", LogWidth/2., LogLength/2., LayerThickness/2.);  
 	G4LogicalVolume *logicLog = new G4LogicalVolume(solidLog, CsIMaterial, "logicLog");
--- src/generator.cc
+++ src/generator.cc
@@ -21,7 +21,7 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 
 	fParticleGun->SetParticlePosition(pos);
 	fParticleGun->SetParticleMomentumDirection(mom);
-	fParticleGun->SetParticleMomentum(200. * MeV);
+	fParticleGun->SetParticleMomentum(100. * MeV);
 	fParticleGun->SetParticleDefinition(particle);
 
 	fParticleGun->GeneratePrimaryVertex(anEvent);
