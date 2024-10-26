b42d7fcdf1e0392576d5a2111c455d5df1196fad
María <maria.rd02@gmail.com>
2024-10-26 20:19:33 +0200

    Added a particle gun

--- src/generator.cc
+++ src/generator.cc
@@ -12,5 +12,17 @@ MyPrimaryGenerator::~MyPrimaryGenerator()
 
 void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 {
+	G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
+	G4String particleName = "gamma";
+	G4ParticleDefinition *particle = particleTable->FindParticle("gamma");
 
+	G4ThreeVector pos(0., 0., -4.*m);
+	G4ThreeVector mom(0., 0., 1.);
+
+	fParticleGun->SetParticlePosition(pos);
+	fParticleGun->SetParticleMomentumDirection(mom);
+	fParticleGun->SetParticleMomentum(200. * MeV);
+	fParticleGun->SetParticleDefinition(particle);
+
+	fParticleGun->GeneratePrimaryVertex(anEvent);
 }
