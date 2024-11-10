301f0b96ebaf24f2f0c0a45efebebb13251dd164
María <maria.rd02@gmail.com>
2024-11-10 23:45:09 +0100

    Modified the data analysis file to work with the new output

--- Analysis/data_split.py
+++ Analysis/data_split.py
@@ -2,20 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 file = "../build/PMT.txt"
-
-energy, time, scintillator, event, photon_ID = np.loadtxt(file, usecols=[0, 1, 2, 3, 4], skiprows=1, unpack=True)
+scintillator, energy, event= np.loadtxt(file, usecols=[0, 1, 2], skiprows=1, unpack=True)
 
 # Separate through events
 num_events = int(max(event) + 1)
 num_data = len(energy)
 
 energy_split = [ [] for i in range(num_events) ]
-time_split = [ [] for i in range(num_events) ]
 scintillator_split = [ [] for i in range(num_events) ]
 
 for i in range(num_data):
 	energy_split[int(event[i])].append(energy[i])
-	time_split[int(event[i])].append(time[i])
 	scintillator_split[int(event[i])].append(scintillator[i])
 
 # Calculate the total energy in each scintillator
@@ -34,7 +31,7 @@ for event_idx in range(num_events):
 for i, event_energy in enumerate(energy_scintillator):
     print(f"Event {i+1}:")
     for scint, total_energy in event_energy.items():
-        print(f"  Scintillator {scint}: Total Energy = {total_energy/10**6:.2f} MeV")
+        print(f"  Scintillator {scint}: Total Energy = {total_energy:.2f} eV")
 
 # Calculate the total energy deposit per event
 energy_event = []
@@ -43,12 +40,10 @@ for i in range(num_events):
 	energy_event.append(sum(energy_split[i]))
 
 for i, event_energy in enumerate(energy_event):
-	print(f"Event {i}: energy = {event_energy/10**(6)} MeV")
+	print(f"Event {i + 1}: energy = {event_energy} MeV")
 
 event = 0
-grid_size = 10
-energy_matrix = np.zeros((grid_size, grid_size))
-
+energy_matrix = np.zeros((8, 12))
 
 for i in list(energy_scintillator[event].keys()):
     if i < 100:
@@ -57,11 +52,10 @@ for i in list(energy_scintillator[event].keys()):
         energy_matrix[y, x] = energy_scintillator[event][i]
 
 # Representar la matriz de energía con imshow
-plt.imshow(energy_matrix, cmap='hot', interpolation='nearest')
-plt.colorbar(label="Energía depositada")
-plt.xlabel("Posición x")
-plt.ylabel("Posición y")
-plt.title("Distribución de energía en centelleadores")
+plt.imshow(energy_matrix)
+plt.colorbar(label="Energy deposit (eV)")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.show()
 
 
